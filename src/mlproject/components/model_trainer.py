56ecd791328403e59161e88c07e500ee1d2fdfdb
Debtirtha Ghosh <debtirthaghosh2004@gmail.com>
2025-10-02 11:31:38 +0530

    All at once!!

---
+++ src/mlproject/components/model_trainer.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+from mlproject.utils.logger import logger
+from sklearn.linear_model import ElasticNet
+import joblib
+from mlproject.entity.config_entity import ModelTrainerConfig
+
+class ModelTrainer:
+    def __init__(self, config: ModelTrainerConfig):
+        self.config = config
+
+    def train(self):
+        train_data = pd.read_csv(self.config.train_data_path)
+        test_data = pd.read_csv(self.config.test_data_path)
+
+        train_x = train_data.drop(["quality"], axis=1)
+        test_x = test_data.drop(["quality"], axis=1)
+        train_y = train_data[["quality"]]
+        test_y = test_data[["quality"]]
+
+        lr = ElasticNet(alpha=self.config.alpha, l1_ratio=self.config.l1_ratio, random_state=self.config.random_state)
+        lr.fit(train_x, train_y)
+
+        joblib.dump(lr, os.path.join(self.config.root_dir, self.config.model_name))
+        
